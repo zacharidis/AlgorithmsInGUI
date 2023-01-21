f47e1ace10d7d106cad01c34c298c4c59b5c548e
Georgios Zacharidis <zageo@outlook.com>
2023-01-21 22:13:13 +0200

    Added folder for classes

---
+++ AlgorithmsInGUI/Classes/FizzBuzz.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmsInGUI.Classes
+{
+    internal class FizzBuzz
+    {
+    }
+}
